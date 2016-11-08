045418b775621e079ad6c26ffeaabb25f493ded4
Elikem Adadevoh <elikem@gmail.com>
2016-11-07 20:22:14 -0500

    Redirect book tour buttons to inquiry page

--- jsautoload/footer/altair-footer.js
+++ jsautoload/footer/altair-footer.js
@@ -1,9 +1,11 @@
 jQuery(document).ready(function($) {
   var tourButton = $('a.ow-button-hover');
-
   if (tourButton.find('span').text().trim() == 'VIEW TOURS') {
-    console.log('true');
-
     tourButton.attr('href', '/asia-a-la-carte-tours/');
   }
+
+  var bookTour = jQuery('.ow-button-hover[href="#book-tour"]');
+  jQuery(bookTour).click(function () {
+    window.location.href = '/request-a-catalog/';
+  })
 });
