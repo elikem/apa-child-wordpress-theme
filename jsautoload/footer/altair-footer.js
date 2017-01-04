a35eaaf5b1894bff275b4c2b0e657981f71848d9
Elikem Adadevoh <elikem@gmail.com>
2017-01-03 19:56:41 -0500

    Add mask to homepage sliders

--- jsautoload/footer/altair-footer.js
+++ jsautoload/footer/altair-footer.js
@@ -5,11 +5,17 @@ jQuery(document).ready(function($) {
   }
 
   var bookTour = $('.ow-button-hover[href="#book-tour"]');
-  jQuery(bookTour).click(function () {
+  $(bookTour).click(function () {
     window.location.href = '/request-a-catalog/';
   });
 
   var newAndNewsLink = '<p style="padding: 10px; text-decoration: underline;"><a href="/new-news/">Click here to read our newsletter</a></p>';
   var newAndNewsNode = $('.sidebar_widget.three .widget_sow-contact-form');
   newAndNewsNode.append(newAndNewsLink);
+
+  if ($(".home")) {
+    revapi35.on('revolution.slide.onloaded', function() {
+      $(".slotholder div").append('<div style="background-color: rgba(0, 0, 0, 0.3); width: 100%; height: 100%;"></div>');
+    });
+  }
 });
